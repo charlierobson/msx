c07c24139206c4f4df5a42fd7c5ef0ce49efb715
sirmorris <charlie_robson@hotmail.com>
2018-07-05 21:25:25 +0100

    some initial exporting

--- brdparse/brdparse.js
+++ brdparse/brdparse.js
@@ -55,15 +55,16 @@ console.log(JSON.stringify(cpld, null, 3));
 
 
 var ledred = pic.connections.filter(c => c.net == '!LED-RED')[0].pinName;
-var ledwhite = pic.connections.filter(c => c.net == '!LED-WHITE')[0].pinName;
-var ledblue = pic.connections.filter(c => c.net == '!LED-BLUE')[0].pinName;
-
 let ledredport = ledred.substring(1,2);
-let ledredpin = ledred.substring(2,3);
+let ledredpin = ledred.substring(2);
+
+var ledwhite = pic.connections.filter(c => c.net == '!LED-WHITE')[0].pinName;
 let ledwhiteport = ledwhite.substring(1,2);
-let ledwhitepin = ledwhite.substring(2,3);
+let ledwhitepin = ledwhite.substring(2);
+
+var ledblue = pic.connections.filter(c => c.net == '!LED-BLUE')[0].pinName;
 let ledblueport = ledblue.substring(1,2);
-let ledbluepin = ledblue.substring(2,3);
+let ledbluepin = ledblue.substring(2);
 
 console.log('#define LED_RED_ON		mPORT'+ledredport+'ClearBits(BIT_'+ledredpin+')');
 console.log('#define LED_RED_OFF		mPORT'+ledredport+'SetBits(BIT_'+ledredpin+')');
@@ -81,3 +82,95 @@ console.log('mPORT'+ledwhiteport+'SetBits(BIT_'+ledwhitepin+');\\');
 console.log('mPORT'+ledwhiteport+'SetPinsDigitalOut(BIT_'+ledwhitepin+');\\');
 console.log('mPORT'+ledblueport+'SetBits(BIT_'+ledbluepin+');\\');
 console.log('mPORT'+ledblueport+'SetPinsDigitalOut(BIT_'+ledbluepin+');');
+
+
+var ppreadname = pic.connections.filter(c => c.net == 'CLIENTREAD')[0].pinName;
+let ppreadpin = ppreadname.substring(2);
+
+var ppwritename = pic.connections.filter(c => c.net == 'CLIENTWRITE')[0].pinName;
+let ppwritepin = ppwritename.substring(2);
+
+var ppstatusname = pic.connections.filter(c => c.net == 'CLIENTSTATUS')[0].pinName;
+let ppstatuspin = ppstatusname.substring(2);
+
+console.log('#define PP_READ_BIT BIT_'+ppreadpin);
+console.log('#define PP_WRITE_BIT BIT_'+ppwritepin);
+console.log('#define PP_STATUS_BIT BIT_'+ppstatuspin);
+
+
+var sdss = pic.connections.filter(c => c.net == '!SS')[0].pinName;
+let sdssport = sdss.substring(1,2);
+let sdsspin = sdss.substring(2);
+var sdcd = pic.connections.filter(c => c.net == '!CD')[0].pinName;
+let sdcdport = sdcd.substring(1,2);
+let sdcdpin = sdcd.substring(2);
+
+console.log('#define SD_SELECT() mPORT'+sdssport+'ClearBits(BIT_'+sdsspin+');');
+console.log('#define SD_DESELECT() mPORT'+sdssport+'SetBits(BIT_'+sdsspin+');');
+console.log('#define SD_CD_BIT BIT_'+sdcdpin);
+console.log('#define SD_CD (mPORT'+sdcdport+'ReadBits(SD_CD_BIT))');
+console.log('#define InitSDPins()\\');
+console.log('mPORT'+sdssport+'SetPinsDigitalOut(BIT_'+sdsspin+');\\');
+console.log('mPORT'+sdcdport+'SetPinsDigitalIn(SD_CD_BIT);');
+
+
+
+var jttdo = pic.connections.filter(c => c.net == 'TDO')[0].pinName;
+let jttdoport = jttdo.substring(1,2);
+let jttdopin = jttdo.substring(2);
+
+var jttdi = pic.connections.filter(c => c.net == 'TDI')[0].pinName;
+let jttdiport = jttdi.substring(1,2);
+let jttdipin = jttdi.substring(2);
+
+var jttck = pic.connections.filter(c => c.net == 'TCK')[0].pinName;
+let jttckport = jttck.substring(1,2);
+let jttckpin = jttck.substring(2);
+
+var jttms = pic.connections.filter(c => c.net == 'TMS')[0].pinName;
+let jttmsport = jttms.substring(1,2);
+let jttmspin = jttms.substring(2);
+
+console.log('#define JTAG_TDO BIT_'+jttdopin);
+console.log('#define JTAG_TCK BIT_'+jttckpin);
+console.log('#define JTAG_TMS BIT_'+jttmspin);
+console.log('#define JTAG_TDI BIT_'+jttdipin);
+console.log('#define InitJTAGPins_Ready()\\');
+console.log('mPORT'+jttdiport+'SetPinsDigitalOut(JTAG_TDI);\\');
+console.log('mPORT'+jttckport+'SetPinsDigitalOut(JTAG_TCK);\\');
+console.log('mPORT'+jttmsport+'SetPinsDigitalOut(JTAG_TMS);\\');
+console.log('mPORT'+jttdoport+'SetPinsDigitalIn(JTAG_TDO);');
+console.log('#define InitJTAGPins_Off()\\');
+console.log('mPORT'+jttdiport+'SetPinsDigitalIn(JTAG_TDI);\\');
+console.log('mPORT'+jttckport+'SetPinsDigitalIn(JTAG_TCK);\\');
+console.log('mPORT'+jttmsport+'SetPinsDigitalIn(JTAG_TMS);\\');
+console.log('mPORT'+jttdoport+'SetPinsDigitalIn(JTAG_TDO);');
+console.log('#define READTDO (mPORT'+jttdoport+'Read() & JTAG_TDO)');
+console.log('#define JTAGSET mPORT'+jttdoport+'SetBits');
+console.log('#define JTAGCLR mPORT'+jttdoport+'ClearBits');
+
+
+var sertx = pic.connections.filter(c => c.net == 'TX-S')[0].pinName;
+let sertxport = sertx.substring(1,2);
+let sertxpin = sertx.substring(2);
+
+var serrx = pic.connections.filter(c => c.net == 'RX-S')[0].pinName;
+let serrxport = serrx.substring(1,2);
+let serrxpin = serrx.substring(2);
+
+console.log('#define InitSerialPins()\\');
+console.log('mPORT'+sertxport+'SetPinsDigitalOut(BIT_'+sertxpin+');\\');
+console.log('mPORT'+serrxport+'SetPinsDigitalIn(BIT_'+serrxpin+');');
+
+
+
+
+for (let conn of cpld.connections) {
+    if (conn.pins.length == 1) {
+        var bg = '';
+        if (conn.pins[0] == 1 || conn.pins[0] == 43 || conn.pins[0] == 44) {
+            bg = ' | BUFG = DATA_GATE';
+        }
+        console.log('NET "'+conn.net+'"  LOC = "P'+conn.pins[0]+'"' + bg + ';');
+    }
+}
